2617b7dc95f189ec20aae81d1f0d74d6eef6693e
Jana Kabelacova <580323@mail.muni.cz>
2026-07-23 23:36:19 +0200

    fixed some problems in ranking class and added new comments

--- src/Ranking.java
+++ src/Ranking.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class Ranking extends JPanel{
@@ -48,6 +49,10 @@ public class Ranking extends JPanel{
     }
 
     private void tryReadFile(String filename) throws IOException {
+        /**
+         * try to read file
+         * @param filename file name
+         */
         this.readFile = new File(filename);
         try {
             this.reader = new Scanner(readFile);
@@ -69,7 +74,11 @@ public class Ranking extends JPanel{
         }
     }
 
-    private String findKey(int a){ // to find nkey to value
+    private String findKey(int a){
+        /**
+         * find key by value
+         * @param a value
+         */
         for(Map.Entry u : map.entrySet()){
             if(Objects.equals(a, u.getValue())){
                 return (String) u.getKey();
@@ -78,9 +87,18 @@ public class Ranking extends JPanel{
         return null;
     }
 
-    private static Integer[] sortValues(){ // sorting too view in order
+    private static Integer[] sortValues(){
+        /**
+         * sort values
+         */
         Integer[] values = new Integer[5];
         int j = 0;
+        if(map.size() > 5){
+            throw new RuntimeException("Too many scores");
+        }
+        if(map.size() < 5){
+            throw new RuntimeException("Not enough scores");
+        }
         for(int i : map.values()){
             values[j] = i;
             j++;
@@ -89,7 +107,10 @@ public class Ranking extends JPanel{
         return values;
     }
 
-    public void viewRank(){ //viewing on JFrame
+    public void viewRank(){
+        /**
+         * view rank via jframe
+         */
         int rank = 1;
         int y = 30;
         Integer[] values = sortValues();
@@ -115,7 +136,13 @@ public class Ranking extends JPanel{
         repaint();
     }
 
-    private static void scoring(Integer[] value, int score, int place){ //add new score
+    private static void scoring(Integer[] value, int score, int place){
+        /**
+         * add new score
+         * @param value sorted values
+         * @param score new score
+         * @param place place on the leaderboard
+         */
         int helpingPoint;
         for(; place < value.length; place++){
             helpingPoint = value[place];
@@ -124,7 +151,11 @@ public class Ranking extends JPanel{
         }
     }
 
-    public void score(int score){ // what is the place
+    public void score(int score){
+        /**
+         * where is the new score on the leaderboard?
+         * @param score new score
+         */
         Integer[] values = sortValues();
         int newPlace = 0;
         boolean newHighScore = false;
@@ -142,15 +173,20 @@ public class Ranking extends JPanel{
     }
     private boolean inScores(String a, Integer[] values){  //is this name in scores
         for(int i : values){
-            if(map.get(a) == i){
+            if(map.get(a).equals(i)){
                 return true;
             }
         }
         return false;
     }
-    private String SameNaming(String name, int number){ // to remove bug with same names
-        for(String a : map.keySet()){
-            if(a.equals(name)){
+    private String SameNaming(String name, int number){
+        /**
+         * if there is a same name, add number to the end of name
+         * @param name name
+         * @param number number
+         */
+        for(String nameKey : map.keySet()){
+            if(nameKey.equals(name)){
                 name = "" + name + number;
                 number++;
                 name = SameNaming(name, number);
@@ -158,7 +194,12 @@ public class Ranking extends JPanel{
         }
         return name;
     }
-    private void newScore(Integer[] values, int score){ //to remove last place and add new name to list
+    private void newScore(Integer[] values, int score) {
+        /**
+         * create new score
+         * @param values sorted values
+         * @param score new score
+         */
         String b = null;
         for(String a : map.keySet()){
             if(!inScores(a, values)){
@@ -172,7 +213,7 @@ public class Ranking extends JPanel{
         if(name == null){
             name = "Anon"; // cannot have name which value is null
         }
-        SameNaming(name, 1); // is there same name? also adds another char
+        SameNaming(name, 1); // is there a same name? also adds another char
         map.put(name, score);
     }
     public void ending() throws IOException { // save score
@@ -187,7 +228,7 @@ public class Ranking extends JPanel{
         reader.close();
         writer.close();
         readFile.delete();
-        Files.move(writeFile.toPath(), writeFile.toPath().resolveSibling("ranking.txt"));
+        Files.move(writeFile.toPath(), writeFile.toPath().resolveSibling("ranking.txt"), StandardCopyOption.REPLACE_EXISTING);
     }
     private void exiting(){
         removeAll();
